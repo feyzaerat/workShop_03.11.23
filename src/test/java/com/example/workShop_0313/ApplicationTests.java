7024bbd973c1b7fd54a37c2f4b5834bf941d3434
Feyza Karanfil Erat <56940562+feyzaerat@users.noreply.github.com>
2023-11-03 15:41:02 +0300

    first commit

---
+++ src/test/java/com/example/workShop_0313/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workShop_0313;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
